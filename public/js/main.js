ba03f5ef8bfc49f3b95d2d5c12c254e50bfb6cd1
unknown <Charlotte Landuyt>
2023-01-10 09:50:11 +0100

    exo

---
+++ public/js/main.js
@@ -0,0 +1,121 @@
+//EXO 1
+
+let firstVariable = "Charlotte";
+console.log(firstVariable);
+
+//exo 1.2
+let vide = "";
+console.log(vide);
+
+
+//exo 1.3
+firstVariable = "Cunéguonde";
+console.log(firstVariable);
+
+
+//exo 1.4
+let elNull = null;
+let newVide = "";
+
+console.log(elNull);
+console.log(newVide);
+
+
+
+
+//EXO 2
+
+let maVariable = "string";
+let maVaria = true;
+let maVar = 10;
+
+console.log(typeof maVariable);
+console.log(typeof maVaria);
+console.log(typeof maVar);
+
+
+//exo 2.1
+let monPays = "Belgique";
+let maCapitale = "Bruxelles";
+let maPhrase = maCapitale + "est la capitale du pays:" + monPays;
+
+console.log(maPhrase);
+
+
+//exo 2.2
+let maPhraseNew = `${maCapitale} est la capitale du pays: ${monPays}`;
+
+console.log(maPhraseNew);
+
+
+//exo 2.3
+let coach = "Kadri";
+let exercice = "exo final";
+let type = "code";
+let etat = "à l'aide";
+let duree = "4";
+
+let elPhrase = `${coach} nous a donné l'${exercice} qu'on ${type} depuis ${duree} jours. Hier, j'ai crié ${etat} après avoir rêve de mon ${type}.`;
+
+console.log(elPhrase);
+
+
+//exo 2.4
+let number = 7;
+let string = "7";
+
+console.log(number = number);
+console.log(number == number);
+console.log(number === number);
+
+//exo 2.5
+let elNumber = 5;
+let elString = "5";
+
+console.log(elNumber + elString);
+
+elString = 5;
+
+console.log(elNumber + elString)
+
+
+
+//EXO 3
+var myPrenom = "Charlotte";
+let myNom = "Landuyt";
+let myAge = 24;
+const againNom = "Landuyt";
+
+console.log(myPrenom);
+console.log(myAge);
+console.log(myNom);
+console.log(againNom);
+
+
+//exo 3.1
+var myPrenom = "Jacqueline";
+
+console.log(myPrenom);
+
+
+//exo 3.2
+//let myAge = 12;
+
+//console.log(newAge);
+
+
+//const againNom = "Gobbers";
+//console.log(againNom);
+
+
+//exo 3.3
+if(true){
+    let premsVar = "prems";
+    var demsVar = "dems";
+}
+
+let premsVar = "quadrems";
+var demsVar = "quintems";
+
+console.log(premsVar);
+console.log(demsVar);
